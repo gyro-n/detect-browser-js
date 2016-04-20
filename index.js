417b5e1eba6faef9a1d2698967be6c3e03c73aaf
kim-yongjin <y_kim@ubicast.com>
2016-04-20 20:09:29 +0900

    Open to npm

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+ * Detect Browser JS
+ * https://github.com/Kim-YongJin/detect-browser-js
+ *
+ * Copyright (c) 2016 YongJin Kim. All rights reserved.
+ */
+
+module.exports = require('./lib/detect-browser');
